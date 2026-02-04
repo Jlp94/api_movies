51f8b18f316a3745dfb07405ae9c0e337470d51b
Jlp94 <jlpcdr94@gmail.com>
2026-02-04 22:42:11 +0100

    error corregido

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const morgan = require('morgan');
-const { mongoose } = require('./database');
+const mongoose = require('./database');
 require('dotenv').config();
 
 // Settings
